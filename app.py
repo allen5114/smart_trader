5bc8e1b9df1a1c7b5b472df67dac51d4fd1ef785
Chuan <chuan.l.hung@gmail.com>
2021-04-23 13:01:43 -1000

    show recommendation trends for the last year

--- app.py
+++ app.py
@@ -1,18 +1,9 @@
 import dash
 import dash_core_components as dcc
 import dash_html_components as html
-import pandas as pd
 import finnhub
 import configparser
-
-from dash.dependencies import Input, Output
-
 import json
-
-#data = pd.read_csv("avocado.csv")
-#data = data.query("type == 'conventional' and region == 'Albany'")
-#data["Date"] = pd.to_datetime(data["Date"], format="%Y-%m-%d")
-#data.sort_values("Date", inplace=True)
 
 # Read configurations
 config = configparser.ConfigParser()
@@ -29,9 +20,6 @@ finnhub_client = finnhub.Client(api_key=config["finnhub.io"]["api_key"])
 # News Sentiment
 #print(finnhub_client.news_sentiment('AAPL'))
 
-# Recommendation trends
-#print(finnhub_client.recommendation_trends('AAPL'))
-
 app = dash.Dash(__name__)
 
 app.layout = html.Div(
@@ -47,12 +35,13 @@ app.layout = html.Div(
         html.Br(),
         dcc.Tabs(id='tabs', value='recommendation-tab', children=[
             dcc.Tab(label='Recommendation Trends', value='recommendation-tab', children=[
-                html.Iframe(id="recommandation",
-                    src="https://widget.finnhub.io/widgets/recommendation?symbol=AAPL",
-                    style={"height": "500px", "width": "80%"})
+                html.Div(id='recommendation-trends-div'),
             ]),
             dcc.Tab(label='Company News', value='company-news-tab', children=[
                 html.Div(id='company-news-div'),
+            ]),
+            dcc.Tab(label='News Sentiment', value='news-sentiment-tab', children=[
+                html.Span(children='to do'),
             ]),
         ]),
         html.Div(id='tabs-content'),
@@ -62,13 +51,8 @@ app.layout = html.Div(
 # import company news script logic
 import company_news
 
-# Symbols Dropdown event 
-@app.callback(
-    Output('recommandation', 'src'),
-    [Input('symbols-dropdown', 'value')])
-def update_recommendation(value):
-    return 'https://widget.finnhub.io/widgets/recommendation?symbol={}'.format(value)
-
+# import recommendation trends logic
+import recommendation_trends
 
 if __name__ == "__main__":
     app.run_server(debug=True, port=8000)
---
+++ recommendation_trends.py
@@ -0,0 +1,83 @@
+from __main__ import app
+from __main__ import finnhub_client
+
+import dash_core_components as dcc
+import plotly.graph_objs as go
+from dash.dependencies import Input, Output
+
+from datetime import date
+import datetime
+from datetime import datetime as dt
+
+# Symbols Dropdown event 
+@app.callback(
+    Output('recommendation-trends-div', 'children'),
+    [Input('symbols-dropdown', 'value')])
+def update_recommendation_trends(value):
+    response = finnhub_client.recommendation_trends(value)
+
+    periods = []
+    buy = []
+    hold = []
+    sell = []
+    strongBuy = []
+    strongSell = []
+    
+    today = date.today()
+    delta = datetime.timedelta(days = 365)
+    startDate = today - delta
+    
+    graphTitle = value + " Recommendation Trends"
+    
+    for data in response:
+        dateStr = data["period"]
+        dateObj = dt.strptime(dateStr, "%Y-%m-%d")
+        if dateObj.date() >= startDate:
+            periods.append(data["period"])
+            buy.append(data["buy"])
+            hold.append(data["hold"])
+            sell.append(data["sell"])
+            strongBuy.append(data["strongBuy"])
+            strongSell.append(data["strongSell"])
+    
+    strongBuyTrace = go.Bar(
+        x=periods,
+        y=strongBuy,
+        text = strongBuy,
+        textposition='auto',
+        name='StrongBuy',
+        marker=go.bar.Marker(color='rgb(54, 152, 25)')
+    )  
+    buyTrace = go.Bar(
+        x=periods,
+        y=buy,
+        text = buy,
+        textposition='auto',
+        name='Buy',
+        marker=go.bar.Marker(color='rgb(0, 255, 0)')
+    )
+    holdTrace = go.Bar (
+        x=periods,
+        y=hold,
+        text = hold,
+        textposition='auto',
+        name='Hold',
+        marker=go.bar.Marker(color='rgb(255, 153, 0)')
+    )
+    sellTrace = go.Bar (
+        x=periods,
+        y=sell,
+        text = sell,
+        textposition='auto',
+        name='Sell',
+        marker=go.bar.Marker(color='rgb(233, 148, 148)')
+    )
+    strongSellTrace = go.Bar (
+        x=periods,
+        y=strongSell,        
+        text = strongSell,
+        textposition='auto',
+        name='StrongSell',
+        marker=go.bar.Marker(color='rgb(141, 14, 14)')
+    )
+    return [dcc.Graph(figure=go.Figure(data=[strongSellTrace, sellTrace, holdTrace, buyTrace, strongBuyTrace], layout=go.Layout(title=graphTitle, plot_bgcolor='black', barmode='stack')))]
